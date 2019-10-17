0369bb08e92b6506f71e99d615946a7d1bbc2ca8
chronium <chronium@users.noreply.github.com>
2019-10-17 21:48:49 +0300

    Add parsing tuple, if/else, functions, variables

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,28 @@ pub enum PrimitiveType {
     U32,
     U64,
     U128,
+    USize,
     I8,
     I16,
     I32,
     I64,
     I128,
+    ISize,
     Boolean,
     Char,
     Str,
+    Any,
+    NamedType {
+        name: String,
+        ty: Box<PrimitiveType>,
+    },
     TupleType(Vec<PrimitiveType>),
+    Pointer(Box<PrimitiveType>),
+    Params(Box<Option<PrimitiveType>>),
+    FunctionType {
+        ret_type: Box<PrimitiveType>,
+        param: Box<PrimitiveType>,
+    },
 }
 
 #[derive(Debug)]
@@ -43,28 +56,43 @@ pub enum VarType {
 }
 
 #[derive(Debug)]
-pub enum ParseTree {
+pub enum AST {
     True,
     False,
+    Empty,
     IntLiteral(PrimitiveType, u64),
     CharLiteral(char),
     StringLiteral(String),
-    Unary(UnaryOp, Box<ParseTree>),
-    Binary(BinaryOp, Box<ParseTree>, Box<ParseTree>),
-    Parenthesized(Box<ParseTree>),
-    If(Box<ParseTree>, Box<ParseTree>),
-    IfElse(Box<ParseTree>, Box<ParseTree>),
-    Block(Vec<ParseTree>),
-    Decl(VarType, PrimitiveType, String, Box<Option<ParseTree>>),
-    Assign(Box<ParseTree>, Box<ParseTree>),
-    Index(Box<ParseTree>, Box<ParseTree>),
+    Unary(UnaryOp, Box<AST>),
+    Binary(BinaryOp, Box<AST>, Box<AST>),
+    Parenthesized(Box<AST>),
+    If {
+        condition: Box<AST>,
+        then_block: Box<AST>,
+    },
+    IfElse {
+        if_expr: Box<AST>,
+        else_expr: Box<AST>,
+    },
+    Block(Vec<AST>),
+    Decl(VarType, PrimitiveType, String, Box<Option<AST>>),
+    Assign(Box<AST>, Box<AST>),
+    Index {
+        lhs: Box<AST>,
+        indexer: Box<AST>,
+    },
     Ref(String),
-    Tuple(Vec<ParseTree>),
+    Tuple(Vec<AST>),
+    AddrOf(Box<AST>),
+    Function {
+        name: String,
+        ty: Box<PrimitiveType>,
+    },
 }
 
 peg::parser! {
     grammar lang_parser() for str {
-        use ParseTree;
+        use AST;
 
         rule _() = quiet!{[' ' | '\n' | '\t']*}
 
@@ -74,10 +102,12 @@ peg::parser! {
         rule keywords()
             = "u8" / "u16" / "u32" / "u64" / "u128"
             / "i8" / "i16" / "i32" / "i64" / "i128"
+            / "usize" / "isize"
             / "if" / "else"
-            / "bool" / "char" / "str"
+            / "bool" / "char" / "str" / "any"
             / "true" / "false"
             / "val" / "var" / "const"
+            / "fn"
 
         rule ident() -> String
             = id:$(quiet!{!keywords() ident_key()}) { id.to_string() }
@@ -94,13 +124,19 @@ peg::parser! {
             / "i32" { PrimitiveType::I32 }
             / "i64" { PrimitiveType::I64 }
             / "i128" { PrimitiveType::I128 }
+            / "usize" { PrimitiveType::USize }
+            / "isize" { PrimitiveType::ISize }
+            / "any" { PrimitiveType::Any }
 
         rule ty() -> PrimitiveType
             = prim:int_primitive_type() { prim }
             / "bool" { PrimitiveType::Boolean }
             / "char" { PrimitiveType::Char }
             / "str" { PrimitiveType::Str }
+            / i:ident() _ ":" _ t:ty() { PrimitiveType::NamedType { name: i, ty: box t } }
             / prim:parenthesized(<ty() ** (_ "," _)>) { PrimitiveType::TupleType(prim) }
+            / "*" prim:ty() { PrimitiveType::Pointer(box prim) }
+            / "..." _ t:ty()? { PrimitiveType::Params(box t) }
             / expected!("type")
 
         rule parenthesized<T>(x: rule<T>) -> T = "(" _ v:x() _ ")" { v }
@@ -110,72 +146,88 @@ peg::parser! {
         rule single_quoted<T>(x: rule<T>) -> T = "'" v:x() "'" { v }
         rule bracketed<T>(x: rule<T>) -> T = "[" v:x() "]" { v }
 
-        rule else() -> ParseTree
+        rule else() -> AST
             = "else" _ els:block() { els }
 
-        rule cond() -> ParseTree
+        rule cond() -> AST
             = "if" _ cond:expr() _ then:block() _ els:(else())? {
-                let if_expr = ParseTree::If(box cond, box then);
+                let if_expr = AST::If { condition: box cond, then_block: box then };
                 match els {
                     None => if_expr,
-                    Some(else_cnd) => ParseTree::IfElse(box if_expr, box else_cnd)
+                    Some(else_blck) => AST::IfElse { if_expr: box if_expr, else_expr: box else_blck }
                 }
             }
+        rule assign() -> AST
+            = "=" _ val:expr() { val }
 
-        rule decl() -> ParseTree
-            = "var" _ id:ident() _ ":" _ ty:ty() _ "=" _ val:(expr())? { ParseTree::Decl(VarType::Mutable, ty, id, box val) }
-            / "val" _ id:ident() _ ":" _ ty:ty() _ "=" _ val:(expr())? { ParseTree::Decl(VarType::Immutable, ty, id, box val) }
-            / "const" _ id:ident() _ ":" _ ty:ty() _ "=" _ val:(expr())? { ParseTree::Decl(VarType::Const, ty, id, box val) }
+        rule decl() -> AST
+            = "var" _ id:ident() _ ":" _ ty:ty() _ val:assign()? { AST::Decl(VarType::Mutable, ty, id, box val) }
+            / "val" _ id:ident() _ ":" _ ty:ty() _ val:assign()? { AST::Decl(VarType::Immutable, ty, id, box val) }
+            / "const" _ id:ident() _ ":" _ ty:ty() _ val:assign()? { AST::Decl(VarType::Const, ty, id, box val) }
 
-        rule string_literal() -> ParseTree
-            = "\"" s:$((!"\"" [_])*) "\"" { ParseTree::StringLiteral(s.to_string()) }
+        rule string_literal() -> AST
+            = "\"" s:$((!"\"" [_])*) "\"" { AST::StringLiteral(s.to_string()) }
 
-        rule char_literal() -> ParseTree
-            = c:$(single_quoted(<[_]>)) { ParseTree::CharLiteral(c.chars().nth(1).unwrap()) }
-        rule int_literal() -> ParseTree
-            = n:$(['0'..='9']+) ty:int_primitive_type()? { ParseTree::IntLiteral(ty.unwrap_or(PrimitiveType::I32), n.parse().unwrap()) }
-        rule unary() -> ParseTree
-            = "-" n:atom() { ParseTree::Unary(UnaryOp::Negative, box n) }
-            / "!" n:atom() { ParseTree::Unary(UnaryOp::Negate, box n) }
-        rule atom() -> ParseTree
+        rule char_literal() -> AST
+            = c:$(single_quoted(<[_]>)) { AST::CharLiteral(c.chars().nth(1).unwrap()) }
+        rule int_literal() -> AST
+            = n:$(['0'..='9']+) ty:int_primitive_type()? { AST::IntLiteral(ty.unwrap_or(PrimitiveType::I32), n.parse().unwrap()) }
+        rule unary() -> AST
+            = "-" n:atom() { AST::Unary(UnaryOp::Negative, box n) }
+            / "!" n:atom() { AST::Unary(UnaryOp::Negate, box n) }
+            / "&" n:atom() { AST::AddrOf(box n) }
+        rule atom() -> AST
             = n:(
                 unary() /
                 int_literal() /
                 char_literal() /
                 string_literal()
                 ) { n }
-            / "true" { ParseTree::True }
-            / "false" { ParseTree::False }
-            / n:ident() { ParseTree::Ref(n) }
-            / n:parenthesized(<expr() ** (_ "," _)>) { ParseTree::Tuple(n) }
-        rule expr() -> ParseTree = precedence! {
+            / "true" { AST::True }
+            / "false" { AST::False }
+            / n:ident() { AST::Ref(n) }
+            / n:parenthesized(<(e:expr()? {e.unwrap_or(AST::Empty)}) ** (_ "," _)>) { AST::Tuple(n) }
+            / "fn" _ n:ident() _ param:ty() _ "->" _ ret:ty() {
+                AST::Function {
+                    name: n,
+                    ty: box PrimitiveType::FunctionType {
+                        ret_type: box ret,
+                        param: box param,
+                    }
+                }
+             }
+        rule expr() -> AST = precedence! {
             e:atom() { e }
-            e:parenthesized(<expr()>) { ParseTree::Parenthesized(box e) }
+            e:parenthesized(<expr()>) { AST::Parenthesized(box e) }
             e:cond() { e }
             e:decl() { e }
             --
-            x:(@) _ "+" _ y:@ { ParseTree::Binary(BinaryOp::Add, box x, box y) }
-            x:(@) _ "-" _ y:@ { ParseTree::Binary(BinaryOp::Subtract, box x, box y) }
+            x:(@) _ "+" _ y:@ { AST::Binary(BinaryOp::Add, box x, box y) }
+            x:(@) _ "-" _ y:@ { AST::Binary(BinaryOp::Subtract, box x, box y) }
             --
-            x:(@) _ "*" _ y:@ { ParseTree::Binary(BinaryOp::Multiply, box x, box y) }
-            x:(@) _ "/" _ y:@ { ParseTree::Binary(BinaryOp::Divide, box x, box y) }
+            x:(@) _ "*" _ y:@ { AST::Binary(BinaryOp::Multiply, box x, box y) }
+            x:(@) _ "/" _ y:@ { AST::Binary(BinaryOp::Divide, box x, box y) }
             --
-            x:(@) _ "%" _ y:@ { ParseTree::Binary(BinaryOp::Modulo, box x, box y) }
+            x:(@) _ "%" _ y:@ { AST::Binary(BinaryOp::Modulo, box x, box y) }
             --
-            x:(@) _ "==" _ y:@ { ParseTree::Binary(BinaryOp::Equals, box x, box y) }
-            x:(@) _ "!=" _ y:@ { ParseTree::Binary(BinaryOp::NotEquals, box x, box y) }
+            x:(@) _ "==" _ y:@ { AST::Binary(BinaryOp::Equals, box x, box y) }
+            x:(@) _ "!=" _ y:@ { AST::Binary(BinaryOp::NotEquals, box x, box y) }
             --
-            x:(@) _ "=" _ y:@ { ParseTree::Assign(box x, box y) }
+            x:(@) _ "=" _ y:@ { AST::Assign(box x, box y) }
             --
-            x:(@) _ "[" _ y:expr() _ "]" { ParseTree::Index(box x, box y) }
+            x:(@) _ "[" _ y:expr() _ "]" {
+                AST::Index {
+                    lhs: box x,
+                    indexer: box y
+                }
+            }
         }
 
-        rule line() -> ParseTree
-            = _ line:decl() _ { line }
-            / _ line:expr() _ { line }
+        rule line() -> AST
+            = _ line:expr() _ { line }
 
-        pub rule block() -> ParseTree
-            = _ blck:curly(<line()*>) _ { ParseTree::Block(blck) }
+        pub rule block() -> AST
+            = _ blck:curly(<line()*>) _ { AST::Block(blck) }
             / _ blck:line() _ { blck }
     }
 }
@@ -186,8 +238,11 @@ fn main() {
         lang_parser::block(
             "
 {
-    val tuple: (i32, str, char) = (128i32, \"hello\", '!')
-    const Unit: () = ()
+    fn puts str -> i32
+    fn printf (*u8, ...) -> i32
+
+    fn swap (x: any, y: any) -> (any, any)
+        = (y, x)
 }
         "
         )
