67039c086f4e6ae75f0523e898a8a8b861ce50f2
chronium <chronium@users.noreply.github.com>
2019-11-11 18:18:42 +0200

    Add EOF and InvalidEOF parser tests

--- parser/src/lib.rs
+++ parser/src/lib.rs
@@ -4,7 +4,14 @@ mod ast;
 mod lexer;
 
 use ast::AST;
-use lexer::token::Token;
+use lexer::token::{Token, TokenKind};
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum ParseError {
+  InvalidEOF,
+}
+
+type ParseResult = Result<(), ParseError>;
 
 pub struct Parser<Iter>
 where
@@ -24,7 +31,48 @@ where
       pt: None,
     }
   }
+
+  fn pt(&self) -> &Option<AST> {
+    &self.pt
+  }
+
+  fn parse(&mut self) -> ParseResult {
+    loop {
+      let t = match self.tt.next() {
+        Some(tok) if tok.kind == TokenKind::EOF => break Ok(()),
+        Some(tok) => tok,
+        None => break Err(ParseError::InvalidEOF),
+      };
+    }
+  }
 }
 
 #[cfg(test)]
-mod parser_tests {}
+mod parser_tests {
+  use super::{lexer::Lexer, ParseError, Parser};
+
+  fn tt(code: &'static str) -> Lexer<'static> {
+    let mut lexer = Lexer::new(code);
+    lexer.lex();
+
+    lexer
+  }
+
+  #[test]
+  fn invalid_eof() {
+    let mut parser = Parser::new(vec![].into_iter());
+    let res = parser.parse();
+    assert_eq!(
+      res.expect_err("Expected InvalidEOF"),
+      ParseError::InvalidEOF
+    );
+  }
+
+  #[test]
+  fn eof() {
+    let mut parser = Parser::new(tt("").into_iter());
+    let res = parser.parse();
+
+    assert_eq!(res, Ok(()))
+  }
+}
